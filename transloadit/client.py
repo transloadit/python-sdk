c9bb68f0f0796c2120c6b297809023c6edca448b
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2017-06-22 21:23:59 +0100

    refactor: better argument name

--- transloadit/client.py
+++ transloadit/client.py
@@ -37,21 +37,21 @@ class Transloadit(object):
     def new_assembly(self, params=None):
         return assembly.Assembly(self, options=params)
 
-    def get_assembly(self, assembly_id=None, url=None, params=None):
-        if not (assembly_id or url):
-            raise ValueError("Either 'assembly_id' or 'url' cannot be None.")
+    def get_assembly(self, assembly_id=None, assembly_url=None, params=None):
+        if not (assembly_id or assembly_url):
+            raise ValueError("Either 'assembly_id' or 'assembly_url' cannot be None.")
 
-        url = url if url else '/assemblies/{}'.format(assembly_id)
+        url = assembly_url if assembly_url else '/assemblies/{}'.format(assembly_id)
         return self.request.get(url, params=params)
 
     def list_assemblies(self, params=None):
         return self.request.get('/assemblies', params=params)
 
-    def cancel_assembly(self, assembly_id=None, url=None, data=None):
-        if not (assembly_id or url):
-            raise ValueError("Either 'assembly_id' or 'url' cannot be None.")
+    def cancel_assembly(self, assembly_id=None, assembly_url=None, data=None):
+        if not (assembly_id or assembly_url):
+            raise ValueError("Either 'assembly_id' or 'assembly_url' cannot be None.")
 
-        url = url if url else '/assemblies/{}'.format(assembly_id)
+        url = assembly_url if assembly_url else '/assemblies/{}'.format(assembly_id)
         return self.request.delete(url, data=data)
 
     def get_template(self, template_id, params=None):
