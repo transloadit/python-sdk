487fd51c8b159f3ba69c1e2f1983b395333f9aea
Arnaud Limbourg <arnaud@limbourg.com>
2021-03-27 17:01:27 +0100

    Clarify example

--- examples/image_resize.py
+++ examples/image_resize.py
@@ -1,10 +1,10 @@
 from transloadit.client import Transloadit
 
-tl = Transloadit("TRANSLOADIT_KEY", "TRANSLOADIT_SECRET")
-ass = tl.new_assembly()
-ass.add_file(open("fixtures/lol_cat.jpg", "rb"))
-ass.add_step("resize", "/image/resize", {"width": 70, "height": 70})
-response = ass.create(wait=True)
+client = Transloadit("TRANSLOADIT_KEY", "TRANSLOADIT_SECRET")
+assembly = client.new_assembly()
+assembly.add_file(open("fixtures/lol_cat.jpg", "rb"))
+assembly.add_step("resize", "/image/resize", {"width": 70, "height": 70})
+response = assembly.create(wait=True)
 
 result_url = response.data.get("results").get("resize")[0].get("ssl_url")
 print("Your result:", result_url)
